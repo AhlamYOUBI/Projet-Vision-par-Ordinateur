aae0971c6822eb4bb587ca6ade9d04b77b250cb3
AhlamYOUBI <ahlam.youbi@usmba.ac.ma>
2023-04-13 22:00:33 +0000

    Version 2
    + Rotation + Egalisation + Etirement

--- projetVO.py
+++ projetVO.py
@@ -1,10 +1,11 @@
 
 import tkinter as tk
-from tkinter import filedialog, Canvas
+from tkinter import filedialog, Canvas, simpledialog
 from PIL import Image, ImageTk
 import PIL
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 #from tkinter import*
 
 class VO_MainApplication:
@@ -34,13 +35,13 @@ class VO_MainApplication:
         #SOUS MENU TRANSFORMATION
         transformation_menu = tk.Menu(menuBar, tearoff=0)
         menuBar.add_cascade(label='Transformation', menu=transformation_menu)
-        transformation_menu.add_command(label='Negative', command=self.function)
-        transformation_menu.add_command(label='Rotation', command=self.function)
-        transformation_menu.add_command(label='Redimension', command=self.function)
+        transformation_menu.add_command(label='Negative', command=self.negative)
+        transformation_menu.add_command(label='Rotation', command=self.rotation)
+        transformation_menu.add_command(label='Redimension', command=self.redimensionner)
         transformation_menu.add_command(label='Rectangle', command=self.function)
-        transformation_menu.add_command(label='Histogramme', command=self.function)
-        transformation_menu.add_command(label='Etirement', command=self.function)
-        transformation_menu.add_command(label='Egalisation', command=self.function)
+        transformation_menu.add_command(label='Histogramme', command=self.histogramme)
+        transformation_menu.add_command(label='Etirement', command=self.etirement)
+        transformation_menu.add_command(label='Egalisation', command=self.egalisation)
 
         #SOUS MENU BINARISATION
         binarisation_menu = tk.Menu(menuBar, tearoff=0)
@@ -69,8 +70,8 @@ class VO_MainApplication:
         menuBar.add_cascade(label='Morphologie', menu=morphologie_menu)
         morphologie_menu.add_command(label='Erosion', command=self.erosion)
         morphologie_menu.add_command(label='Dilatation', command=self.dilatation)
-        morphologie_menu.add_command(label='Ouverture', command=self.function)
-        morphologie_menu.add_command(label='Fermeture', command=self.function)
+        morphologie_menu.add_command(label='Ouverture', command=self.ouverture)
+        morphologie_menu.add_command(label='Fermeture', command=self.fermeture)
         morphologie_menu.add_command(label='Filtrage morphologique', command=self.function)
 
         #SOUS MENU SEGMENTATION
@@ -175,6 +176,109 @@ class VO_MainApplication:
             img = np.array(self.original_image)
             self.modified_image = cv2.dilate(src=img,kernel=ES,iterations=1)
             self.display_modified_image()
+
+
+    def ouverture(self) :
+        if self.original_image is not None :
+            ES = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (17, 17))
+            img = np.array(self.original_image)
+            self.modified_image = cv2.morphologyEx(img, cv2.MORPH_OPEN, ES)
+            self.display_modified_image()
+
+
+    def fermeture(self) :
+        if self.original_image is not None :
+            ES = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (17, 17))
+            img = np.array(self.original_image)
+            self.modified_image = cv2.morphologyEx(img, cv2.MORPH_CLOSE, ES)
+            self.display_modified_image()
+
+
+    def negative(self):
+        if self.original_image is not None:
+            img = np.array(self.original_image)
+            if len(img.shape) == 3:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # Appliquer le négatif
+            img_negative = 255 - img
+
+            self.modified_image = img_negative
+            self.display_modified_image()
+
+    def etirement(self, lower_pct=5, upper_pct=95):
+        # l'étirement améliore le contraste local de l'image *en étirant ou réduisant* la plage des niveaux de gris utilisée.
+        if self.original_image is not None:
+            img = np.array(self.original_image)
+            if len(img.shape) == 3:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # Calculer les niveaux de gris minimum et maximum
+            p_lower, p_upper = np.percentile(img, (lower_pct, upper_pct))
+            # Appliquer la transformation linéaire
+            img_etiree = (img - p_lower) * (255 / (p_upper - p_lower))
+            # Clipper les valeurs en dehors de [0, 255]
+            img_etiree = np.clip(img_etiree, 0, 255).astype(np.uint8)
+            
+            self.modified_image = img_etiree
+            self.display_modified_image()
+
+
+    def egalisation(self):
+        #l'égalisation d'histogramme améliore le contraste global de l'image en répartissant les niveaux de gris *de manière uniforme*.
+        if self.original_image is not None:
+            img = np.array(self.original_image)
+            if len(img.shape) == 3:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # Appliquer l'égalisation d'histogramme
+            img_egualise = cv2.equalizeHist(img)
+
+            self.modified_image = img_egualise
+            self.display_modified_image()
+
+
+    def histogramme(self):
+        if self.original_image is not None:
+            if self.original_image is not None:
+                img = np.array(self.original_image)
+                if len(img.shape) == 3:
+                    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+            plt.plot(hist, color='black')
+            plt.xlabel('Niveau de gris')
+            plt.ylabel('Nombre de pixels')
+            plt.show()
+
+    def rotation(self):
+        
+        if self.original_image is not None:
+            angle = simpledialog.askfloat("Rotation", "Entrez l'angle de rotation en degrés : ", parent=self.master)
+            if angle is None:
+                return
+            rows, cols = self.original_image.shape[:2]
+            M = cv2.getRotationMatrix2D((cols/2, rows/2), angle, 1)
+            self.modified_image = cv2.warpAffine(self.original_image, M, (cols, rows))
+            self.display_modified_image()
+
+
+################################################################################
+    def redimensionner(self):
+        if self.original_image is not None:
+            width = simpledialog.askinteger("Redimensionnement", "Entrez la nouvelle largeur : ", parent=self.master)
+            height = simpledialog.askinteger("Redimensionnement", "Entrez la nouvelle hauteur : ", parent=self.master)
+
+            self.resized_img = self.original_image.resize((width, height))
+            self.display_resized_image()
+
+    def display_resized_image(self):
+        # Créer un widget Canvas pour afficher l'image redimensionnée
+        self.canvas_modified_image = tk.Canvas(self.modified_image, width=self.resized_img.width, height=self.resized_img.height)
+        self.canvas_modified_image.pack()
+
+        # Convertir l'image en format Tkinter et l'afficher dans le widget Canvas
+        tk_image = ImageTk.PhotoImage(self.resized_img)
+        self.canvas_modified_image.create_image(0, 0, anchor=tk.NW, image=tk_image)
+        self.canvas_modified_image.image = tk_image
+################################################################################3
 
 root = tk.Tk()
 app = VO_MainApplication(root)
